11eba761b474bce8a82fb12827aa7b90fad1f1aa
Shubham <Shubham@Shubham-PC>
2022-10-14 12:37:24 +0530

    Second Commit

--- MyProject/src/practice/First.java
+++ MyProject/src/practice/First.java
@@ -6,6 +6,7 @@ public class First {
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome! This is my first program.");
+		System.out.println("Om Shree Ganeshay Namahaa!");
 	}
 
 }
